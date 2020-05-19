ce7f8968b2fb900aa7005b2f8ae1d1aba77a2b6d
Graham Hill <gthill97@gmail.com>
2020-05-18 23:39:18 -0600

    Fleshed out the command line interface for the sc command

--- src/main/kotlin/com/meamoria/lexurgy/main.kt
+++ src/main/kotlin/com/meamoria/lexurgy/main.kt
@@ -2,15 +2,56 @@ package com.meamoria.lexurgy
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.multiple
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.path
 
-class Ling : CliktCommand() {
+class Lexurgy : CliktCommand() {
     override fun run() = Unit
 }
 
-class SC : CliktCommand(help="Applies sound changes from CHANGES (a .lsc file) to the words in WORDS (a .wli file)") {
+class SC : CliktCommand(
+    help = "Applies sound changes from CHANGES (a .lsc file) to the words in WORDS (a .wli file). " +
+            "By default, the output file has the suffix \"_ev\" added, but this can be changed with the --out-suffix option."
+) {
+    val changes by argument().path(mustBeReadable = true)
+    val words by argument().path(mustBeReadable = true)
+    val startAt by option(
+        "-a", "--start-at",
+        help = "The rule to start at. Lexurgy will apply this change and all subsequent changes."
+    )
+    val stopBefore by option(
+        "-b", "--stop-before",
+        help = "The rule to stop before. Lexurgy will apply all changes before the specified rule."
+    )
+    val inSuffix by option(
+        "-i", "--in-suffix",
+        help = "The suffix on the input filename WORDS. " +
+                "E.g. if this is \"old\" and WORDS is \"foo.wli\", Lexurgy will read words from foo_old.wli instead."
+    )
+    val outSuffix by option(
+        "-o", "--out-suffix",
+        help = "The suffix on the output filename. " +
+                "E.g. if this is \"new\" and WORDS is \"foo.wli\", Lexurgy will write the changed words to foo_new.wli. " +
+                "Defaults to \"ev\"."
+    ).default("ev")
+    val debugWords by option(
+        "-d", "--debug-word",
+        help = "A word to record debugging information for. Can be provided multiple times to specify several words to debug."
+    ).multiple()
+
     override fun run() {
-        echo("Called sc")
+        echo("Called sc:")
+        echo("Changes = $changes")
+        echo("Words = $words")
+        echo("Start at $startAt")
+        echo("Stop before $stopBefore")
+        echo("In suffix = $inSuffix")
+        echo("Out suffix = $outSuffix")
+        echo("Debug words = $debugWords")
     }
 }
 
-fun main(args: Array<String>) = Ling().subcommands(SC()).main(args)
+fun main(args: Array<String>) = Lexurgy().subcommands(SC()).main(args)
