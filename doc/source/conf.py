d1e9e32fc2490caa27ba1bf989757a99e8e4ec3d
Graham Hill <gthill97@gmail.com>
2021-02-05 20:59:27 -0700

    Updated version number

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Graham Hill'
 author = 'Graham Hill'
 
 # The full version, including alpha/beta/rc tags
-release = '0.11.0'
+release = '0.12.0'
 
 
 # -- General configuration ---------------------------------------------------
