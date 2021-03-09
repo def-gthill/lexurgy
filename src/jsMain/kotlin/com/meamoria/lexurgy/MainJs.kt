3e124f91c3f7184ceb5eeefdb9fa75093d8151a0
Graham Hill <gthill97@gmail.com>
2021-03-08 23:32:17 -0700

    Deliver the final words to the web app separately from the history (and errors)

--- src/jsMain/kotlin/com/meamoria/lexurgy/MainJs.kt
+++ src/jsMain/kotlin/com/meamoria/lexurgy/MainJs.kt
@@ -6,6 +6,7 @@ import kotlinx.browser.document
 import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLInputElement
 import org.w3c.dom.HTMLTextAreaElement
+import org.w3c.dom.events.Event
 
 fun main() {
     if (document.title.isEmpty()) return
@@ -13,6 +14,8 @@ fun main() {
     val inputField = document.getElementById("input") as HTMLTextAreaElement
     val changeField = document.getElementById("changes") as HTMLTextAreaElement
     val outputField = document.getElementById("output") as HTMLTextAreaElement
+    val historyField = document.getElementById("history") as? HTMLTextAreaElement
+    val errorField = document.getElementById("error") as? HTMLTextAreaElement
 
     val startAtCheckbox = document.getElementById("start_at_enabled") as? HTMLInputElement
     val startAtField = document.getElementById("start_at") as? HTMLInputElement
@@ -20,7 +23,10 @@ fun main() {
     val stopBeforeCheckbox = document.getElementById("stop_before_enabled") as? HTMLInputElement
     val stopBeforeField = document.getElementById("stop_before") as? HTMLInputElement
 
-    fun output(text: String) { outputField.value = text }
+    fun output(element: HTMLTextAreaElement, text: String) {
+        element.value = text
+        element.dispatchEvent(Event("input"))
+    }
 
     val button = document.getElementById("apply") as HTMLButtonElement
     button.addEventListener("click", {
@@ -30,22 +36,32 @@ fun main() {
             val startAtRule = startAtField?.value.takeIf { startAtCheckbox?.checked == true }
             val stopBeforeRule = stopBeforeField?.value.takeIf { stopBeforeCheckbox?.checked == true }
             val soundChanger = SoundChanger.fromLsc(changes)
-            val outputWords = unpackIntermediates(
-                inputWords,
+            val stages =
                 soundChanger.changeWithIntermediates(
                     inputWords,
                     startAt = startAtRule,
                     stopBefore = stopBeforeRule,
                 )
-            )
-            output(outputWords.joinToString("\n"))
+            val outputWords = stages.getValue(null)
+            val intermediates = unpackIntermediates(inputWords, stages)
+            if (historyField == null) {
+                output(
+                    outputField,
+                    intermediates.zip(outputWords) {
+                            intermediate, outputWord -> intermediate + outputWord
+                    }.joinToString("\n")
+                )
+            } else {
+                output(historyField, intermediates.joinToString("\n"))
+                output(outputField, outputWords.joinToString("\n"))
+            }
         } catch (e: Exception) {
-            output(e.message.toString())
+            output(errorField ?: outputField, e.message.toString())
         }
     })
 }
 
 private fun unpackIntermediates(inputWords: List<String>, stages: Map<String?, List<String>>): List<String> =
     makeStageComparisons(
-        listOf(inputWords) + stages.filterKeys { it != null }.values + listOf(stages.getValue(null))
+        listOf(inputWords) + stages.filterKeys { it != null }.values + listOf(inputWords.map { "" })
     )
