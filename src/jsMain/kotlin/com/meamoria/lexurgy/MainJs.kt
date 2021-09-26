dd58bd0818b0b6a19918137b91b6e9ddd5761b95
Graham Hill <gthill97@gmail.com>
2021-09-25 23:46:00 -0600

    Better web app integration

--- src/jsMain/kotlin/com/meamoria/lexurgy/MainJs.kt
+++ src/jsMain/kotlin/com/meamoria/lexurgy/MainJs.kt
@@ -1,9 +1,44 @@
+@file:Suppress("unused")
+
 package com.meamoria.lexurgy
 
 import com.meamoria.lexurgy.sc.SoundChanger
 import com.meamoria.lexurgy.sc.makeStageComparisons
 
-fun main() {
-    SoundChanger.toString()
-    ::makeStageComparisons.toString()
+@JsName("SoundChanger")
+class JsSoundChanger(val sc: SoundChanger) {
+    @JsName("change")
+    fun change(
+        words: Array<String>,
+        startAt: String? = null,
+        stopBefore: String? = null,
+        debugWords: Array<String> = emptyArray(),
+        romanize: Boolean = true,
+        debugCallback: (String) -> Unit = {},
+    ): Array<SoundChangerResult> =
+        sc.changeWithIntermediates(
+            words.asList(),
+            startAt,
+            stopBefore,
+            debugWords.asList(),
+            romanize,
+            debugCallback,
+        ).map { (name, words) ->
+            SoundChangerResult(name, words.toTypedArray())
+        }.toTypedArray()
+
+    val ruleNames: Array<String>
+        get() = sc.rules.map { it.name }.toTypedArray()
+
+    companion object {
+        @JsName("fromLsc")
+        fun fromLsc(code: String): JsSoundChanger =
+            JsSoundChanger(SoundChanger.fromLsc(code))
+    }
 }
+
+class SoundChangerResult(val name: String?, val words: Array<String>)
+
+@JsName("scMakeStageComparisons")
+fun scMakeStageComparisons(wordListSequence: Array<Array<String>>): Array<String> =
+    makeStageComparisons(wordListSequence.map { it.asList() }).toTypedArray()
