4714b5509c3818828cdfad28cf3b2bc457dfa4ca
Graham Hill <gthill97@gmail.com>
2022-10-09 15:21:08 -0600

    New version number for experimental CLI features

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Graham Hill'
 author = 'Graham Hill'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.4'
+release = '1.1.5-dev'
 
 
 # -- General configuration ---------------------------------------------------
