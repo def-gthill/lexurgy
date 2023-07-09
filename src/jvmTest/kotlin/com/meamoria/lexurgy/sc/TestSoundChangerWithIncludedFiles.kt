b763bbd5b76c7a59142ccf1df5254f65ee7b2ff7
Graham Hill <gthill97@gmail.com>
2023-07-09 10:51:17 -0600

    Fix include when the main path is just a filename

--- src/jvmTest/kotlin/com/meamoria/lexurgy/sc/TestSoundChangerWithIncludedFiles.kt
+++ src/jvmTest/kotlin/com/meamoria/lexurgy/sc/TestSoundChangerWithIncludedFiles.kt
@@ -4,6 +4,8 @@ import com.meamoria.lexurgy.dumpList
 import com.meamoria.lexurgy.loadList
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import java.io.File
+import java.io.FileInputStream
 import java.nio.file.FileSystems
 import java.nio.file.Path
 import kotlin.time.ExperimentalTime
@@ -11,6 +13,10 @@ import kotlin.time.ExperimentalTime
 @Suppress("unused")
 @ExperimentalTime
 class TestSoundChangerWithIncludedFiles : StringSpec({
+    fun String.toPath(): Path {
+        return FileSystems.getDefault().getPath(this)
+    }
+
     fun pathOf(vararg pathComponents: String): Path =
         FileSystems.getDefault().getPath("test", *pathComponents)
 
@@ -27,9 +33,27 @@ class TestSoundChangerWithIncludedFiles : StringSpec({
         return outDir
     }
 
-    val changer = soundChangerFromLscFile(pathOf("muipidan_includer.lsc"))
+    fun copyToCwd(filenames: List<String>) {
+        for (filename in filenames) {
+            val sourcePath = pathOf(filename)
+            val targetFile = File(filename)
+
+            FileInputStream(sourcePath.toFile()).use { inputStream ->
+                targetFile.outputStream().use { outputStream ->
+                    inputStream.copyTo(outputStream)
+                }
+            }
+        }
+    }
+
+    fun deleteFromCwd(filenames: List<String>) {
+        for (filename in filenames) {
+            File(filename).delete()
+        }
+    }
 
     "A sound changer with included files should be able to process some wordlists" {
+        val changer = soundChangerFromLscFile(pathOf("muipidan_includer.lsc"))
         val outDir = prepareOutDir("basic")
         changer.changeFiles(
             listOf(pathOf("ptr_test_1.wli"), pathOf("ptr_test_2.wli")),
@@ -37,5 +61,36 @@ class TestSoundChangerWithIncludedFiles : StringSpec({
         )
         listFrom(outDir, "ptr_test_1_ev.wli") shouldBe listFrom("ptr_test_1_ev_expected.wli")
         listFrom(outDir, "ptr_test_2_ev.wli") shouldBe listFrom("ptr_test_2_ev_expected.wli")
+    }
+
+    "A sound changer can include a file in the same directory as the main file using just the filename" {
+        val changer = soundChangerFromLscFile(pathOf("muipidan_includer_relative.lsc"))
+        val outDir = prepareOutDir("basic")
+        changer.changeFiles(
+            listOf(pathOf("ptr_test_1.wli"), pathOf("ptr_test_2.wli")),
+            outDir = outDir,
+        )
+        listFrom(outDir, "ptr_test_1_ev.wli") shouldBe listFrom("ptr_test_1_ev_expected.wli")
+        listFrom(outDir, "ptr_test_2_ev.wli") shouldBe listFrom("ptr_test_2_ev_expected.wli")
+    }
+
+    "A sound changer can include a file when the change file path is just a filename" {
+        val filenames = listOf(
+            "muipidan_includer_relative.lsc",
+            "muipidan_included.lsc",
+            "muipidan_definitions.lsc",
+            "ptr_test_1.wli"
+        )
+        copyToCwd(filenames)
+
+        try {
+            val changer = soundChangerFromLscFile(
+                "muipidan_includer_relative.lsc".toPath()
+            )
+            changer.changeFiles(listOf("ptr_test_1.wli".toPath()))
+            loadList("ptr_test_1_ev.wli".toPath()) shouldBe listFrom("ptr_test_1_ev_expected.wli")
+        } finally {
+            deleteFromCwd(filenames + "ptr_test_1_ev.wli")
+        }
     }
 })
