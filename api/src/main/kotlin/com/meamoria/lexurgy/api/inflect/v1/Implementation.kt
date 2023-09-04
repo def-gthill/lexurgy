d870439c92fccbe7b6cff3f8b4adb3a15e9f20a7
Graham Hill <gthill97@gmail.com>
2023-09-04 17:04:30 -0600

    Have serializable models convert themselves

--- api/src/main/kotlin/com/meamoria/lexurgy/api/inflect/v1/Implementation.kt
+++ api/src/main/kotlin/com/meamoria/lexurgy/api/inflect/v1/Implementation.kt
@@ -4,7 +4,7 @@ import com.meamoria.lexurgy.inflect.*
 import com.meamoria.lexurgy.word.Word
 
 fun runInflect(request: Request): Response {
-    val rules = rulesFromRequest(request.rules)
+    val rules = request.rules.toRules()
     val result = try {
         rules.inflectAll(
             *stemsAndCategoriesFromRequest(request.stemsAndCategories)
@@ -16,40 +16,13 @@ fun runInflect(request: Request): Response {
     return SuccessResponse(formsToResponse(result))
 }
 
-private fun rulesFromRequest(requestRules: RequestCategoryTree): CategoryTree {
-    return when (requestRules) {
-        is RequestForm -> Form(requestRules.form)
-        is RequestFormulaForm -> Form(formulaFromRequest(requestRules.formula))
-        is RequestCategorySplit -> CategorySplit(
-            requestRules.branches.mapValues {
-                rulesFromRequest(it.value)
-            }
-        )
-    }
-}
-
 private fun stemsAndCategoriesFromRequest(
     requestStemsAndCategories: List<RequestStemAndCategories>
 ): Array<StemAndCategories> {
     return requestStemsAndCategories
-        .map(::stemAndCategoriesFromRequest)
+        .map { it.toStemAndCategories() }
         .toTypedArray()
 }
-
-private fun stemAndCategoriesFromRequest(
-    requestStemAndCategories: RequestStemAndCategories
-): Pair<Word, Set<String>> {
-    val (stem, categories) = requestStemAndCategories
-    return Word(stem) to categories.toSet()
-}
-
-private fun formulaFromRequest(formula: RequestFormula): Formula =
-    when (formula) {
-        is RequestStem -> Stem
-        is RequestForm -> Fixed(formula.form)
-        is RequestConcat -> Concat(formula.parts.map(::formulaFromRequest))
-    }
-
 
 private fun formsToResponse(forms: List<Word>): List<String> {
     return forms.map { it.string }
--- api/src/main/kotlin/com/meamoria/lexurgy/api/inflect/v1/Request.kt
+++ api/src/main/kotlin/com/meamoria/lexurgy/api/inflect/v1/Request.kt
@@ -1,5 +1,7 @@
 package com.meamoria.lexurgy.api.inflect.v1
 
+import com.meamoria.lexurgy.inflect.*
+import com.meamoria.lexurgy.word.Word
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -10,39 +12,61 @@ data class Request(
 )
 
 @Serializable
-sealed interface RequestCategoryTree
+sealed interface RequestCategoryTree {
+    fun toRules(): CategoryTree
+}
 
 @Serializable
 @SerialName("form")
-data class RequestForm(val form: String) : RequestCategoryTree, RequestFormula
+data class RequestForm(val form: String) : RequestCategoryTree, RequestFormula {
+    override fun toRules(): Form = Form(form)
+
+    override fun toFormula(): Fixed = Fixed(form)
+}
 
 @Serializable
 @SerialName("formula")
 data class RequestFormulaForm(
     val formula: RequestFormula
-) : RequestCategoryTree
+) : RequestCategoryTree {
+    override fun toRules(): Form = Form(formula.toFormula())
+}
 
 @Serializable
 @SerialName("split")
 data class RequestCategorySplit(
     val branches: Map<String, RequestCategoryTree>
-) : RequestCategoryTree
+) : RequestCategoryTree {
+    override fun toRules(): CategorySplit =
+        CategorySplit(branches.mapValues { it.value.toRules() })
+}
 
 @Serializable
-data class RequestStemAndCategories(
-    val stem: String,
-    val categories: List<String>
-)
-
-@Serializable
-sealed interface RequestFormula
+sealed interface RequestFormula {
+    fun toFormula(): Formula
+}
 
 @Serializable
 @SerialName("stem")
-object RequestStem : RequestFormula
+object RequestStem : RequestFormula {
+    override fun toFormula(): Stem = Stem
+}
 
 @Serializable
 @SerialName("concat")
 data class RequestConcat(
     val parts: List<RequestFormula>
-) : RequestFormula
+) : RequestFormula {
+    override fun toFormula(): Concat =
+        Concat(parts.map { it.toFormula() })
+}
+
+@Serializable
+data class RequestStemAndCategories(
+    val stem: String,
+    val categories: List<String>
+) {
+    fun toStemAndCategories(): StemAndCategories {
+        return Word(stem) to categories.toSet()
+    }
+}
