838c3ccafaae469256acd9be606b985b33f388c4
Graham Hill <gthill97@gmail.com>
2023-09-14 09:04:40 -0600

    Update version number

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.meamoria"
-version = "1.3.1"
+version = "1.3.2"
 
 repositories {
     mavenCentral()
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.jvm")
 }
 
-val versionNumber = "1.3.1"
+val versionNumber = "1.3.2"
 
 group = "com.meamoria"
 version = versionNumber
