b83d2aee9da01195e1cc21dfbe700ad3eae9dc07
Graham Hill <gthill97@gmail.com>
2023-10-08 13:47:01 -0600

    Demonstrate the polling technique to avoid timeouts

--- api/src/main/kotlin/com/meamoria/lexurgy/api/Routing.kt
+++ api/src/main/kotlin/com/meamoria/lexurgy/api/Routing.kt
@@ -4,17 +4,50 @@ import com.meamoria.lexurgy.api.inflect.v1.runInflectV1
 import com.meamoria.lexurgy.api.sc.v1.runScV1
 import com.meamoria.lexurgy.api.sc.v1.runScV1Validate
 import com.meamoria.lexurgy.sc.SoundChanger
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
+import java.util.concurrent.atomic.AtomicInteger
 
 fun Application.configureRouting() {
+    val backgroundCounter = AtomicInteger()
     routing {
         get("/") {
             call.respondText("You've reached the Lexurgy API! The root doesn't do anything though.")
         }
         get("/version") {
             call.respondText((SoundChanger::class).java.`package`.implementationVersion)
+        }
+        post("/background") {
+            val thread = Thread {
+                backgroundCounter.set(0)
+                for (i in 1..20) {
+                    Thread.sleep(10)
+                    val counter = backgroundCounter.incrementAndGet()
+                    println("Working, counter = $counter")
+                }
+            }
+            thread.start()
+            call.response.status(HttpStatusCode.Accepted)
+            call.respond(buildJsonObject {
+                put("url", "/background/placeholderForJobId")
+            })
+        }
+        get("/background/placeholderForJobId") {
+            val counter = backgroundCounter.get()
+            if (counter >= 20) {
+                call.respond(buildJsonObject {
+                    put("status", "working")
+                })
+            } else {
+                call.respond(buildJsonObject {
+                    put("status", "done")
+                    put("result", 42)
+                })
+            }
         }
         post("/scv1") {
             call.runScV1()
--- api/src/test/kotlin/com/meamoria/lexurgy/api/ApplicationTest.kt
+++ api/src/test/kotlin/com/meamoria/lexurgy/api/ApplicationTest.kt
@@ -1,7 +1,14 @@
 package com.meamoria.lexurgy.api
 
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
+import kotlinx.coroutines.delay
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.int
+import kotlinx.serialization.json.jsonPrimitive
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -19,4 +26,29 @@ class ApplicationTest {
             assertEquals(HttpStatusCode.OK, status)
         }
     }
+
+    @Test
+    fun pollingTest_RunsInTheBackground() = testApi {
+        var pollingUrl: String
+        client.post("/background").apply {
+            assertEquals(HttpStatusCode.Accepted, status)
+            pollingUrl = bodyAsJson().getValue("url").jsonPrimitive.content
+        }
+        var done = false
+        while (!done) {
+            client.get(pollingUrl).apply {
+                assertEquals(HttpStatusCode.OK, status)
+                val status = bodyAsJson().getValue("status").jsonPrimitive.content
+                if (status == "done") {
+                    done = true
+                    val result = bodyAsJson().getValue("result").jsonPrimitive.int
+                    assertEquals(42, result)
+                }
+            }
+            delay(50)
+        }
+    }
+
+    suspend fun HttpResponse.bodyAsJson(): JsonObject =
+        Json.decodeFromString<JsonObject>(bodyAsText())
 }
